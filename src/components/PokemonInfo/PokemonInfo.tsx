be6839adc10ba00f0ada61260b9f54acd18cb90e
ChrisCatsa <162459037+ChrisCatsa@users.noreply.github.com>
2024-05-28 17:07:43 +0200

    Pokemon Information (#2)
    Added Pokemon info component

---
+++ src/components/PokemonInfo/PokemonInfo.tsx
@@ -0,0 +1,31 @@
+import React, { useMemo } from "react";
+import { useGetPokemon } from "../../hooks/useGetPokemon";
+import { useParams } from "react-router-dom";
+import { getMainPokemonType } from "../../utils/getMainPokemonType";
+import { capitalizerFirstLetter } from "../../utils/capitalizeFirstLetter";
+import { convertLbsToKg } from "../../utils/convertLbsToKg";
+import { convertInchesToCm } from "../../utils/convertinchesToCm";
+import { PokemonSprites } from "../PokemonSprites/PokemonSprites";
+
+export const PokemonInfo = () => {
+    const  {pokemonName} = useParams();
+    const { pokemonData } = useGetPokemon(pokemonName);
+    const mainType = useMemo(() => pokemonData && getMainPokemonType(pokemonData), [pokemonData]);
+    return (
+        <div className="flex flex-row justify-between shadow-lg bg-gray-100 rounded-lg">
+            <div className={`${mainType}-background w-72 h-72 rounded-l-lg items-center`}>
+                <img 
+                src={pokemonData?.sprites?.front_default}
+                alt={pokemonData?.name ?? ""}
+                className="mx-auto w-72 h-72"
+                />
+            </div>
+            <div className="flex flex-col grow p-5 gap-3">
+                <h1 className="text-3xl">{capitalizerFirstLetter(pokemonData?.name ?? "")}</h1>
+                <span>{`Weight: ${convertLbsToKg(pokemonData?.weight ?? 0)} kg`}</span>
+                <span>{`Height: ${convertInchesToCm(pokemonData?.height ?? 0)} Cm`}</span>
+                <PokemonSprites pokemonName={pokemonName} />
+            </div>
+        </div>
+    );
+};
