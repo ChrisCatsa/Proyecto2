1ef45a5895adfc18df09cfd82487d82d7d631dc5
ChrisCatsa <162459037+ChrisCatsa@users.noreply.github.com>
2024-06-03 15:03:44 +0200

    Favorite View and Navigation
    Added favorte view and navigation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,38 +1,46 @@
 import React from "react";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 
 const Pokedex = React.lazy(() => import("../views/Pokedex"));
 const PokemonProfile = React.lazy(() => import("../views/PokemonProfile"));
 const PokemonByType = React.lazy(() => import("../views/PokemonType"));
+const FavoritePokemon = React.lazy(() => import("../views/PokemonByFavorte"));
 
 const AppRoutes = () => (
-    <Routes>
-        <Route 
-        path="/" 
-        element={
-            <React.Suspense fallback={<div>Loading...</div>}>
-                <Pokedex />
-            </React.Suspense>
-        }
-        />
-        <Route 
-        path="/pokemon/:pokemonName" 
-        element={
-            <React.Suspense fallback={<div>Loading...</div>}>
-                <PokemonProfile />
-            </React.Suspense>
-        }
-        />
-        <Route 
-        path="/type/:typeName" 
-        element={
-            <React.Suspense fallback={<div>Loading...</div>}>
-                <PokemonByType />
-            </React.Suspense>
-        }
-        />
-    </Routes>
+  <Routes>
+    <Route
+      path="/"
+      element={
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <Pokedex />
+        </React.Suspense>
+      }
+    />
+    <Route
+      path="/pokemon/:pokemonName"
+      element={
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <PokemonProfile />
+        </React.Suspense>
+      }
+    />
+    <Route
+      path="/type/:typeName"
+      element={
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <PokemonByType />
+        </React.Suspense>
+      }
+    />
+    <Route
+      path="/favorite"
+      element={
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <FavoritePokemon />
+        </React.Suspense>
+      }
+    />
+  </Routes>
 );
-
 
 export default AppRoutes;
