25274cdb948556de86d41fb17559092bf79a9a06
ChrisCatsa <162459037+ChrisCatsa@users.noreply.github.com>
2024-06-09 11:23:18 +0200

    Search Feature (#5)
    Added search feature

---
+++ src/components/shared/Button/SearchButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useSearchStore } from "../../../store/useSearchStore"
+
+
+export const SearchButton = () => {
+    const openModal = useSearchStore((state) => state.openModal);
+
+    return (
+        <button onClick={openModal}>Search</button>
+    )
+}; 
